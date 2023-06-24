edb8781274b299ef04c389e9e4c2cdc34ca23d10
Joseph <pmyjgs@nottingham.ac.uk>
2023-06-24 13:19:21 +0100

    Fix boundaries

--- MarkovModels/common.py
+++ MarkovModels/common.py
@@ -506,10 +506,11 @@ def fit_model(mm, data, times=None, starting_parameters=None,
             rates_1 = rates_func(parameters, Vs[0])
             rates_2 = rates_func(parameters, Vs[1])
 
-            if max(rates_1.max(), rates_2.max()) < 1.67e-5:
+            # Boundaries taken from 'Four Ways to Fit an Ion Channel Model'
+            if min(rates_1.max(), rates_2.max()) < 1.67e-5:
                 return False
 
-            if min(rates_1.max(), rates_2.max()) < 1e-3:
+            if max(rates_1.max(), rates_2.max()) > 1e3:
                 return False
 
             if max([p for i, p in enumerate(parameters) if i != mm.GKr_index]) > 1e5:
